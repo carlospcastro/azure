8d5487fcba1666ffe549acb276cadb6873d95ef1
Carlos Castro <carlospcastro@gmail.com>
2021-01-19 22:57:35 +0100

    Update get-vm-info to get vm state

--- network/get-vm-info.sh
+++ network/get-vm-info.sh
@@ -11,3 +11,7 @@ read -p 'Subscription ID: ' SubscriptionID
 az account set -s $SubscriptionID
 
 az vm list --query '[*].{name: name, resourceGroup: resourceGroup, Status: tags.Status, DevOwnerEmail: tags.DevOwnerEmail}' -o table
+
+echo " Now, please wait until get the VM STATE, it's take time "
+
+az vm list -d -o table
